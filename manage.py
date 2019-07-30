6458a7c97177f9a9567fc2959c8c400c7c6f7d90
Dimple K H <dimple.kh.1697@gmail.com>
2019-07-30 19:40:45 +0530

    Updated files

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fullThrottle.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fullthrottle.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
